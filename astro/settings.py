0df3f6dcf6d87413ca0d8e1c0adcfa2ab0c4481b
Keshav <keshavmohta09@gmail.com>
2023-05-26 15:53:36 +0530

    add socal links

--- astro/settings.py
+++ astro/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ["DEBUG"] == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
